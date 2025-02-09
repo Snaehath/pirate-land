c63716404cf80a9f72def77e2d8140c58a7b67de
Suraj Vijay <surajcpp115@gmail.com>
2025-02-09 16:14:14 +0530

    setup - neobrutalism, shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 /* eslint-disable unicorn/filename-case */
+
+import { Button } from "./components/ui/button";
+
 const App: React.FC = () => (
-    <div><h1>Vanakkam</h1></div>
+    <div><Button>Vanakkam</Button></div>
   );
 
 export default App;
