9ca92126a839dd9fa1672ca264601644266c6f1a
Suraj Vijay <surajcpp115@gmail.com>
2025-02-10 00:43:37 +0530

    bg checking

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 /* eslint-disable unicorn/filename-case */
 
 import { Button } from "./components/ui/button";
+import { cn } from "./lib/utils";
 
 const App: React.FC = () => (
-    <div><Button>Vanakkam</Button></div>
-  );
+  <div
+    className={cn(
+      "bg-cover w-screen h-screen bg-[url(./assets/images/map-bg.png)]",
+      "bg-no-repeat",
+    )}
+  >
+    <h1 className="font-pirate-kids text-3xl">Pirate Land</h1>
+    <Button>Vanakkam</Button>
+    <Button>Vanakkam</Button>
+  </div>
+);
 
 export default App;
